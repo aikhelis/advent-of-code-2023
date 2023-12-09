53c869b6bd4b5560357cc5a1369cc9dd7c19f860
aikhelis <alex.ikhelis@imgarena.com>
2023-12-09 13:35:23 +0000

    day9

---
+++ js/09/mirage.js
@@ -0,0 +1,25 @@
+const {readFileAndSplitLines} = require('../lib/readFile');
+const testInput   = readFileAndSplitLines('./js/09/test.txt').map( a => a.split(' ').map(Number) );
+const puzzleInput = readFileAndSplitLines('./js/09/input.txt').map( a => a.split(' ').map(Number) );
+
+const is0 = (n) => (n===0);
+
+function leftAndRightNumbersInSeq(seq) {
+    let delta = seq.map((v, i, vector) => 
+        (vector[i-1] === undefined) ? undefined : vector[i] - vector[i-1]
+    );
+    delta.forEach((v,i,vector) => {if(v===undefined) vector.shift()});
+    const [left, right] = (delta.every(is0)) ? [0,0] : leftAndRightNumbersInSeq(delta);
+    return [seq[0] - left, seq[seq.length-1] + right];
+}
+
+function leftAndRightCheckSum(input){
+    return input.reduce((sum, line) => {
+        const [left, right] = leftAndRightNumbersInSeq(line);
+        return [sum[0]+left, sum[1]+right]; 
+    }, [0,0]);
+}
+
+
+console.log( leftAndRightCheckSum(testInput) );
+console.log( leftAndRightCheckSum(puzzleInput) );
