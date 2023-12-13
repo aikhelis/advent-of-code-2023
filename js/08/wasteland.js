820284e8ed3c4e5697fcdc4b988f8ce93cf7d73c
aikhelis <alex.ikhelis@imgarena.com>
2023-12-13 12:09:32 +0000

    day8 play around a bit

--- js/08/wasteland.js
+++ js/08/wasteland.js
@@ -4,25 +4,36 @@ const testInput2   = readFileLines('./js/08/test2.txt');
 const testInput3   = readFileLines('./js/08/test3.txt');
 const puzzleInput  = readFileLines('./js/08/input.txt');
 
+class Node {
+    L; R;
+    constructor(l, r) {this.L = l; this.R = r;}
+}
+
 function parse(lines){
     let map = {};
     input.forEach( line => {
         nodes = line.match( /^(\S{3}) = \((\S{3}), (\S{3})\)$/ );
-        if(nodes) map[nodes[1]] = {L: nodes[2], R: nodes[3]};  
+        if(nodes) map[nodes[1]] = new Node(nodes[2], nodes[3]);  
     });
     return [lines[0], map];
 }
 
 function fromStartToEnd(instructions, map, startLabel, endLabelTrailing){
     console.log(`From ${startLabel} to ${endLabelTrailing}`);
-    let step = 0;
-    let label = startLabel;
+    if(instructions.length === 0) {console.log('Missing instructions!'); return null;}
+    let step = 0, label = startLabel;
+    console.log(`first step ${step}: ${label} = {${map[label]['L']}, ${map[label]['R']}} - taking ${instructions[0]}`);
     while(true){
-        if(instructions.length === 0) {console.log('Missing instructions!'); break;}
-        if(label.endsWith(endLabelTrailing)) {console.log(`step ${step}, label ${label}`); break;}
+        const prevLabel = label;
         label = map[label][instructions[ step % instructions.length ]];
+        if(label.endsWith(endLabelTrailing)) {
+            console.log(`final step ${step}: ${prevLabel} = {${map[prevLabel].L}, ${map[prevLabel].R}} - taking ${instructions[step % instructions.length]}`);
+            console.log(`stopping on step ${step++} at: ${label} = {${map[label].L}, ${map[label].R}} - looking ${instructions[step % instructions.length]}`);
+            break;
+        }
         step++;
     }
+    console.log(`ran ${step} steps and iterated through instructions ${step / instructions.length} times`);
     return step;
 }
 
@@ -36,12 +47,15 @@ const fromAAAtoZZZ = () => fromStartToEnd(instructions, map, 'AAA', 'ZZZ');
 
 function fromxxAToxxZ(){
     let startLabels = Object.keys(map).filter(label => label.endsWith('A'));
-    const stepCounts = startLabels.map(label => fromStartToEnd(instructions, map, label, 'Z'))
+    const stepCounts = startLabels.map(label => fromStartToEnd(instructions, map, label, 'Z'));
     return leastCommonMultiple(stepCounts);
 }
 
 
 const input = puzzleInput;
 const [instructions, map] = parse(input);
-console.log(fromAAAtoZZZ());
-console.log(fromxxAToxxZ());
+console.log('instructions length: ' + instructions.length);
+console.log();
+console.log('answer: ' + fromAAAtoZZZ());
+console.log();
+console.log('answer: ' + fromxxAToxxZ());
